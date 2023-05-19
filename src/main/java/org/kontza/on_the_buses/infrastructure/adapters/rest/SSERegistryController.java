b1c299d212adc56ed84b9fe38622a80c219fa104
Juha Ruotsalainen <juha.ruotsalainen@ambientia.fi>
2023-05-19 10:05:36 +0300

    Trying out a three hour timeout

--- src/main/java/org/kontza/on_the_buses/infrastructure/adapters/rest/SSERegistryController.java
+++ src/main/java/org/kontza/on_the_buses/infrastructure/adapters/rest/SSERegistryController.java
@@ -3,6 +3,7 @@ package org.kontza.on_the_buses.infrastructure.adapters.rest;
 import lombok.extern.slf4j.Slf4j;
 import org.kontza.on_the_buses.domain.api.NotifierService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,26 +19,44 @@ import java.util.Optional;
 @RequestMapping("/sse")
 @Slf4j
 public class SSERegistryController {
+    public static final String EVENT_REGISTERED = "REGISTERED";
+    public static final String EVENT_UPDATE = "UPDATE";
+    public static final String EVENT_COMPLETE = "COMPLETE";
+    public static final String OK = "OK";
+    public static final long THREE_HOURS = 10800000l;
     @Value("${on-the-buses.sse-timeout}")
     private long sseTimeout;
-    private Map<String, SseEmitter> clients = new HashMap<>();
-    private NotifierService notifierService;
+    private final Map<String, SseEmitter> clients = new HashMap<>();
+    private final NotifierService notifierService;
 
     public SSERegistryController(NotifierService notifierService) {
         this.notifierService = notifierService;
     }
 
+    @GetMapping("/unregister")
+    public ResponseEntity<String> unregisterClient(@RequestParam final String clientId) throws IOException {
+        log.info(">>> Unregister '{}'", clientId);
+        var emitter = clients.remove(clientId);
+        if (emitter != null) {
+            emitter.send(SseEmitter
+                .event()
+                .id(String.valueOf(System.currentTimeMillis()))
+                .name(EVENT_COMPLETE)
+                .data(""));
+            emitter.complete();
+        }
+        return ResponseEntity.ok(OK);
+    }
+
     @GetMapping("/register")
-    public SseEmitter registerClient(@RequestParam final String clientId) {
-        var emitter = new SseEmitter(sseTimeout);
+    public ResponseEntity<SseEmitter> registerClient(@RequestParam final String clientId) throws IOException {
+        var emitter = new SseEmitter(THREE_HOURS);
         if (clients.containsKey(clientId)) {
             log.info(">>> {} was already registered. Completing the previous registration.", clientId);
             clients.get(clientId).complete();
         }
         emitter.onTimeout(() -> {
-            log.error(">>> '{}' emitter timed out", clientId);
-            clients.remove(clientId);
-            emitter.complete();
+            log.warn(">>> '{}' emitter timed out", clientId);
         });
         emitter.onError(err -> {
             log.error(">>> '{}' emitter error:", clientId, err);
@@ -49,16 +68,28 @@ public class SSERegistryController {
             clients.remove(clientId);
         });
         clients.put(clientId, emitter);
-        log.info(">>> Register '{}'", clientId);
-        return emitter;
+        log.info(">>> Registered '{}'", clientId);
+        emitter.send(SseEmitter
+            .event()
+            .id(String.valueOf(System.currentTimeMillis()))
+            .name(EVENT_REGISTERED)
+            .data(""));
+        return ResponseEntity.ok(emitter);
     }
 
     @GetMapping("/update")
     public void update(@RequestParam final String reason) {
-        clients.values().forEach(sseEmitter -> {
+        log.info(">>> Got an update request '{}'", reason);
+        clients.entrySet().forEach(entry -> {
             try {
-                log.info(">>> Got an update request '{}'", reason);
-                sseEmitter.send(reason);
+                String clientId = entry.getKey();
+                SseEmitter sseEmitter = entry.getValue();
+                log.info(">>> Notifying '{}'", clientId);
+                sseEmitter.send(SseEmitter
+                    .event()
+                    .id(String.valueOf(System.currentTimeMillis()))
+                    .name(EVENT_UPDATE)
+                    .data(reason));
                 notifierService.notifier(Optional.of(reason), Optional.of(sseTimeout));
             } catch (IOException e) {
                 log.error(">>> Sending '{}' failed:", reason, e);
--- vue-client/src/components/EventView.vue
+++ vue-client/src/components/EventView.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="grid">
-    <div class="divider">Event Log</div>
-    <textarea class="event-log" rows="25" cols="80" readonly v-model="events"></textarea>
+  <div class="grid grid-cols-3">
+    <div class="divider col-span-3">Event Log</div>
+    <textarea class="event-log col-span-3" rows="25" cols="80" readonly v-model="events"></textarea>
+    <div class="empty-row col-span-3"></div>
+    <input v-model="state.clientId" />
     <button class="btn-primary" @click="register">Register to server</button>
-    <div class="divider">Event to send</div>
+    <button class="btn-secondary" @click="unregister">Unregister from server</button>
+    <div class="divider col-span-3">Event to send</div>
     <input v-model="state.reason" @keyup.enter="send" />
     <button class="btn-primary" @click="send">Send update to server</button>
   </div>
 </template>
 <script setup>
-import { computed, reactive, ref, watch } from 'vue'
+import { computed, ref, reactive, watch } from 'vue'
 import { EventSourcePolyfill } from 'event-source-polyfill'
+import { fetchEventSource, EventStreamContentType } from '@microsoft/fetch-event-source'
+
+class RetriableError extends Error {}
+class FatalError extends Error {}
 
 const logEvent = (message) => {
   const timestamp = new Date().toLocaleTimeString('fi', {
@@ -24,13 +31,13 @@ const logEvent = (message) => {
 const state = reactive({
   reason: 'Senkin huithapeli!',
   eventArray: [],
-  es: null
+  clientId: import.meta.env.VITE_CLIENT_ID,
+  retry: true
 })
 logEvent('Welcome to the machine!')
 const events = computed(() => {
   return state.eventArray.join('\n')
 })
-let es = ref(null)
 
 watch(
   state.eventArray,
@@ -41,42 +48,136 @@ watch(
   },
   { deep: true }
 )
+const MS = 'MS'
+const mode = ref(null)
 
 const register = () => {
-  if (state.es) {
-    logEvent('Already registered')
-    return
-  }
   logEvent('Registering...')
-  let initialEs = new EventSourcePolyfill('/api/register/?clientId=Johnny')
-  let listener = (event) => {
-    switch (event.type) {
-      case 'open':
-        es.value = initialEs
-        logEvent('Registered...')
-        break
-      case 'error':
-        logEvent('Error...')
-        state.es = null
-        break
-      case 'message':
-        logEvent(`Message: ${event.data}`)
-        break
+  state.retry = true
+  if (mode.value === MS) {
+    fetchEventSource(`/api/register/?clientId=${state.clientId}`, {
+      openWhenHidden: true,
+      async onopen(response) {
+        if (response.ok && response.headers.get('content-type') === EventStreamContentType) {
+          logEvent('Registered... ' + JSON.stringify(response))
+          return // everything's good
+        } else if (response.status >= 400 && response.status < 500 && response.status !== 429) {
+          // client-side errors are usually non-retriable:
+          logEvent('Registration failed... ' + JSON.stringify(response))
+          throw new FatalError()
+        } else {
+          logEvent('Registration failed, can be retried... ' + JSON.stringify(response))
+          throw new RetriableError()
+        }
+      },
+      onmessage(msg) {
+        // if the server emits an error message, throw an exception
+        // so it gets handled by the onerror callback below:
+        if (msg.event === 'FatalError') {
+          logEvent('Message error: ' + JSON.stringify(msg))
+          throw new FatalError(msg.data)
+        } else {
+          switch (msg.event) {
+            case 'REGISTERED':
+              logEvent('Client registered')
+              break
+            case 'UPDATE':
+              logEvent('Update: ' + JSON.stringify(msg.data))
+              break
+            case 'COMPLETE':
+              logEvent('Completed')
+              state.retry = false
+              break
+            default:
+              logEvent('Message: ' + JSON.stringify(msg))
+              break
+          }
+        }
+      },
+      onclose() {
+        // if the server closes the connection unexpectedly, retry:
+        if (state.retry) {
+          logEvent('Server closed connection, retry')
+          throw new RetriableError()
+        } else {
+          logEvent('Close on completed')
+        }
+      },
+      onerror(err) {
+        if (err instanceof FatalError) {
+          logEvent('Non-retryable error: ' + JSON.stringify(err))
+          throw err // rethrow to stop the operation
+        } else {
+          // do nothing to automatically retry. You can also
+          // return a specific retry interval here.
+          logEvent('Retryable error: ' + JSON.stringify(err))
+        }
+      }
+    })
+  } else {
+    let initialEs = new EventSourcePolyfill(`/api/register/?clientId=${state.clientId}`, {
+      heartbeatTimeout: 250000
+    })
+    let listener = (event) => {
+      switch (event.type) {
+        case 'open':
+          if (state.retry) {
+            logEvent('Opened')
+          } else {
+            logEvent('Closing the event source')
+            initialEs.close()
+          }
+          break
+        case 'error':
+          if (event.status >= 500) {
+            logEvent('Error... ' + JSON.stringify(event))
+            initialEs.close()
+          }
+          break
+        case 'message':
+          logEvent(`Message: ${event.data}`)
+          break
+        default:
+          logEvent(`Unknown: ${JSON.stringify(event)}`)
+          break
+      }
     }
+    initialEs.addEventListener('open', listener)
+    initialEs.addEventListener('message', listener)
+    initialEs.addEventListener('error', listener)
+    initialEs.addEventListener('REGISTERED', () => {
+      logEvent('Client registered')
+    })
+    initialEs.addEventListener('UPDATE', (payload) => {
+      logEvent('Update: ' + JSON.stringify(payload.data))
+    })
+    initialEs.addEventListener('COMPLETE', () => {
+      logEvent('Client completed')
+      initialEs.close()
+    })
   }
-  initialEs.addEventListener('open', listener)
-  initialEs.addEventListener('message', listener)
-  initialEs.addEventListener('error', listener)
 }
 const send = () => {
   logEvent(`Sending '${state.reason}'...`)
-  fetch(`/api/update/?reason=${state.reason}`).then(() => {
-    logEvent('... sent')
-  })
+  fetch(`/api/update/?reason=${state.reason}`, { method: 'GET' })
+    .then(() => logEvent('... sent'))
+    .catch((error) => logEvent('Update failed: ' + JSON.stringify(error)))
+}
+const unregister = () => {
+  logEvent('Unregistering...')
+  fetch(`/api/unregister/?clientId=${state.clientId}`)
+    .then(() => {
+      state.retry = false
+      logEvent('... done')
+    })
+    .catch((error) => logEvent('Unregister failed: ' + JSON.stringify(error)))
 }
 </script>
 <style scoped>
 .event-log {
   font-family: monospace;
+}
+.empty-row {
+  height: 1rem;
 }
 </style>
